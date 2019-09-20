5dd37fc2c5e491e801585dc09fdfc4950aa00ba5
Jirawat Boonkumnerd <jo06942@gmail.com>
2019-09-20 22:19:40 +0700

    Init project

---
+++ src/main/java/me/ntsd/bouncycastlebenchmark/BouncycastleBenchmarkApplication.java
@@ -0,0 +1,13 @@
+package me.ntsd.bouncycastlebenchmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BouncycastleBenchmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BouncycastleBenchmarkApplication.class, args);
+	}
+
+}
